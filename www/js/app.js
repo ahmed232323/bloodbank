d53792fabdcb4fcf36d7168af6be840bce35ce11
Ahmed Nabil <a7med.nabil.y@hotmail.com>
2015-02-06 00:36:18 +0300

    new hope
    adding angular wizard on donor registration form

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 
+               'angular-underscore', 'mgo-angular-wizard'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -80,6 +80,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       views: {
         'tab-donaters': {
           templateUrl: 'templates/donaters-reg.html'
+//          controller: 'RegDonaterCtrl'
+        }
+      }
+    })
+    .state('tab.donaters-reg2', {
+      url: '/donaters/register2',
+      views: {
+        'tab-donaters': {
+          templateUrl: 'templates/donaters-reg2.html'
 //          controller: 'RegDonaterCtrl'
         }
       }
